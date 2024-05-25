5c26704df77b1a74cf57735a1733eb592599bd05
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-05-25 14:09:11 +0200

    Rewrite(rewrite): start adding left menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
-	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '$lib/components/ui/sonner';
+  import '../app.css'
+  import { ModeWatcher } from 'mode-watcher'
+  import { Toaster } from '$lib/components/ui/sonner'
+  import { Button } from '@lib/components/ui/button'
+  import { Menu } from 'lucide-svelte'
+
+  import { Sheet, SheetTrigger } from '@lib/components/ui/sheet'
 </script>
 
 <ModeWatcher />
 <Toaster />
+
+<nav>
+  <Sheet>
+    <SheetTrigger><Button variant="link"><Menu /></Button></SheetTrigger>
+  </Sheet>
+</nav>
+
 <slot></slot>
 
 <style></style>
