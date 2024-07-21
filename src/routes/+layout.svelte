f8cb8375b1e4dd54ddcdb91b9f2514a84297d13a
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-07-21 21:50:50 +0200

    Tauri2(tauri2): fixing issue with imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,9 @@
   import '../app.css'
   import { ModeWatcher } from 'mode-watcher'
   import { Toaster } from '$lib/components/ui/sonner'
-  // import { invoke } from '@tauri-apps/api/core'
   import { toast } from 'svelte-sonner'
 
   let { children } = $props()
-  // document.addEventListener('DOMContentLoaded', () => {
-  //   // This will wait for the window to load, but you could
-  //   // run this function on whatever trigger you want
-  //   invoke('close_splashscreen')
-  // })
 </script>
 
 <ModeWatcher />
